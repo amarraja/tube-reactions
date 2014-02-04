415e2aa64069d56d7f313876a6d62a1ebe22fc22
Amar Raja <amarsuperstar@gmail.com>
2014-02-04 21:56:43 +0000

    Use EJS

--- app.js
+++ app.js
@@ -49,7 +49,12 @@ function notifySentiments(){
 
 var app = express();
 
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req, res){
+	res.render('index');
+});
 
 app.listen(process.env.PORT || 3000);
 
